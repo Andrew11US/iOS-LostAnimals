70a769fb57393fd3f22d1d4bb21e69510db322a5
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-05-20 21:01:19 +0200

    Added nested scroll view and images collection with page control

--- LostAnimals/DetailVC.swift
+++ LostAnimals/DetailVC.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class DetailVC: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var horizontalScrollView: UIScrollView!
+    @IBOutlet weak var verticalScrollView: UIScrollView!
+    @IBOutlet weak var verticalContentView: UIView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    // MARK: - Variables
+    private var images: [UIImage] = [UIImage(named: "t1")!, UIImage(named: "t2")!, UIImage(named: "t3")!, UIImage(named: "test")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        horizontalScrollView.delegate = self
+        populateHorizontalScrollView()
+    }
+    
+    private func populateHorizontalScrollView() {
+        for i in 0..<images.count {
+            let imageView = UIImageView()
+            imageView.contentMode = .scaleAspectFill
+            imageView.image = images[i]
+            let xPosition = self.view.frame.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPosition, y: 0, width: self.view.frame.width, height: CGFloat(215))
+            
+            horizontalScrollView.contentSize.width = horizontalScrollView.frame.width * CGFloat(i + 1)
+            horizontalScrollView.addSubview(imageView)
+        }
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        verticalScrollView.bringSubviewToFront(pageControl)
     }
     
 
+}
+
+// MARK: - UIScrollView delegate
+extension DetailVC: UIScrollViewDelegate {
+    // Show changing pages on PageControl
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x / view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+    }
 }
