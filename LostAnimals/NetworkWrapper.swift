b89d51d89bb46eb4526f26e863a4449bf4aa9822
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-05-25 23:00:41 +0200

    Getting started with network wrapper

--- LostAnimals/NetworkWrapper.swift
+++ LostAnimals/NetworkWrapper.swift
@@ -9,23 +9,32 @@
 import Alamofire
 
 struct NetworkWrapper {
-    static func getRates(pair: (from:String,to:String), completion: @escaping (Double) -> Void) {
-        let url = "https://api.exchangerate-api.com/v4/latest/\(pair.from)"
+//    static func getRates(pair: (from:String,to:String), completion: @escaping (Double) -> Void) {
+//        let url = "https://api.exchangerate-api.com/v4/latest/\(pair.from)"
+//
+//        AF.request(url).validate().responseJSON { response in
+//            print(response.value ?? " ")
+//            if let dict = response.value as? Dictionary<String, AnyObject> {
+//                if let rates = dict["rates"] as? Dictionary<String, AnyObject> {
+//                    if let rate = rates[pair.to] as? Double {
+//                        print("Rate: ", rate)
+//                        completion(rate)
+//                    }
+//                } else {
+//                    print("Unable to find currency pair.(to:)")
+//                }
+//            }
+//        }
+//
+//    }
+    
+    static func getAds(with completion: @escaping ()->()) {
+        let url = "api_url"
         
         AF.request(url).validate().responseJSON { response in
-            print(response.value ?? " ")
-            if let dict = response.value as? Dictionary<String, AnyObject> {
-                if let rates = dict["rates"] as? Dictionary<String, AnyObject> {
-                    if let rate = rates[pair.to] as? Double {
-                        print("Rate: ", rate)
-                        completion(rate)
-                    }
-                } else {
-                    print("Unable to find currency pair.(to:)")
-                }
-            }
+            print(response.value ?? "x")
+            completion()
         }
-        
     }
     
 }
