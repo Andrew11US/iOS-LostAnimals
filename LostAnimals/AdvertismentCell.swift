d7849c7594adcae72b2066880bc4c19e357f59e5
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-06-03 00:03:20 +0200

    UI changes to adCell

--- LostAnimals/AdvertismentCell.swift
+++ LostAnimals/AdvertismentCell.swift
@@ -14,10 +14,13 @@ class AdvertismentCell: UITableViewCell {
     @IBOutlet weak var badgeLbl: UILabel!
     @IBOutlet weak var nameLbl: UILabel!
     @IBOutlet weak var locationLbl: UILabel!
+    @IBOutlet weak var baseView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        baseView.layer.cornerRadius = 12
+        badgeLbl.layer.cornerRadius = badgeLbl.bounds.height / 2
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -31,5 +34,13 @@ class AdvertismentCell: UITableViewCell {
         self.badgeLbl.text = ad.adType
         self.nameLbl.text = ad.animalName
         self.locationLbl.text = ad.town
+        
+        switch AdType(rawValue: ad.adType) {
+        case .lost: badgeLbl.layer.backgroundColor = UIColor.red.cgColor
+        case .found: badgeLbl.layer.backgroundColor = UIColor.green.cgColor
+        case .adoption: badgeLbl.layer.backgroundColor = UIColor.yellow.cgColor
+        default:
+            badgeLbl.layer.backgroundColor = UIColor.red.cgColor
+        }
     }
 }
