1db8d73ce08e9bec5dbf8e58e1d3004f15dceb39
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-06-07 18:42:24 +0200

    minor fixes

--- LostAnimals/Advertisment.swift
+++ LostAnimals/Advertisment.swift
@@ -26,28 +26,8 @@ struct Advertisment: Codable {
     var chipNumber: String
     var distingMarks: String
     var description: String
-    
-//    init(id: Int, state: String, adType: String, animalType: String, animalName: String, date: String, town: String, district: String, street: String, phone: String, chipNumber: Int, description: String, imageUrl: String) {
-//        self.id = id
-//        self.state = adType
-//        self.adType = adType
-//        self.animalType = animalType
-//        self.animalName = animalName
-//        self.date = date
-//        self.town = town
-//        self.district = district
-//        self.street = street
-//        self.phone = phone
-//        self.chipNumber = chipNumber
-//        self.description = description
-//        self.imageUrl = imageUrl
-//    }
-    
 
-    
-
-    
-//    // MARK: - Initialize from DataSnapshot
+    // MARK: - Initialize from DataSnapshot
     init(adType: AdType, dict: Dictionary<String, AnyObject>) {
         self.adType = adType.rawValue
         self.animalName = dict["name"] as? String ?? ""
@@ -74,21 +54,4 @@ struct Advertisment: Codable {
         }
         print(imageURLs)
     }
-//
-//    // MARK: - Returns transaction like Dictionary<String, AnyObject>
-//    func getDictionary() -> [String : AnyObject] {
-//        return [
-//            "id" : self.id,
-//            "name" : self.name,
-//            "type" : self.type,
-//            "category" : self.category,
-//            "currencyCode" : self.currencyCode,
-//            "unifiedCurrencyCode" : self.unifiedCurrencyCode,
-//            "originalAmount" : self.originalAmount,
-//            "unifiedAmount" : self.unifiedAmount,
-//            "dateCreated" : self.dateCreated,
-//            "walletName" : walletName,
-//            "walletID" : self.walletID,
-//            ] as [String : AnyObject]
-//    }
 }
